d6b90d6863d0e883f94997dd1155ab645f9bfcd2
marczenn <marcusareulius@gmail.com>
2016-06-10 16:02:50 -0600

    Finished tutorial

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,3 +1,4 @@
+import{Meteor} from 'meteor/meteor';
 import{Template} from 'meteor/templating';
 import{ReactiveDict} from 'meteor/reactive-dict';
 
@@ -8,6 +9,7 @@ import './body.html';
 
 Template.body.onCreated(function bodyOnCreated(){
   this.state = new ReactiveDict();
+  Meteor.subscribe('tasks');
 })
 
 Template.body.helpers({
@@ -37,10 +39,7 @@ Template.body.events({
     const text = target.text.value;
 
     // Insert a task into the Collection
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-    });
+    Meteor.call('tasks.insert', text);
 
     // Clear form
     target.text.value = '';
@@ -54,3 +53,11 @@ Template.body.events({
 // ReactiveDicts are reactive data stores for the client
 
 // Until now, we have stored all of our state in collections, and the view updated automatically when we modified the data inside these collections. This is because Mongo.Colleciton is recognized by Meteor as a reactive data source, meaning Meteor knows when the data inside has changed. ReactiveDict is the same way, but not synced with the server like collections are. This makes a ReactiveDict a convenient place to store temporary UI state like the 'checkbox' above. Just like with collections, we don't have to write any extra code for the template to update when the ReactiveDict variable changes - just calling instance.state.get(...) inside the help is enough.
+
+// Automatic accounts UI
+
+// If our ass has the account-ui package, all we have to do to add a login dropdown is include the loginButtons template with {{> loginButtons}}. This dropdown detects which login methods have been added to the app and displays the appropriate controls. In our case, the only enabled login method is accounts-password, so the dropdown displays a password field. If you are adventurous, you can add the accounts-facebook package to enable Facebook login in your app - the Facebook button will automatically appear in the dropdown.
+
+// Getting information about the logged-in user
+
+// In your HTML, you can use the built-in {{currentUser}} helper to check if a user is logged in and get information about them. For example, {{currentUser.username}} will display the logged in user's username. In your JavaScript code, you can use Meteor.userId() to get the current users' _id, or Meteor.user() to get the whole user document.
--- imports/ui/task.js
+++ imports/ui/task.js
@@ -1,18 +1,40 @@
+import{Meteor} from 'meteor/meteor';
 import{Template} from 'meteor/templating';
 
 import{Tasks} from '../api/tasks.js';
 
 import './task.html';
 
+Template.task.helpers({
+  isOwner(){
+    return this.owner === Meteor.userId();
+  },
+});
+
 Template.task.events({
   'click .toggle-checked'(){
     // Set the checked property to the opposite of its current value
-    Tasks.update(this._id, {
-      $set: {checked: ! this.checked},
-    });
+    Meteor.call('tasks.setChecked', this._id, !this.checked);
   },
 
   'click .delete'(){
-    Tasks.remove(this._id);
+    Meteor.call('tasks.setPrivate', this._id, !this.private);
+  },
+
+  'click .toggle-private'(){
+    Meteor.call('tasks.setPrivate', this._id, !this.private);
   },
 });
+
+
+// Optimistic UI
+
+// So why do we want to define our methods on the client and on the server? We do this to enable a feature we call optimistic UI.
+
+// When you call a method on the client using Meteor.call, two things happen in parallel:
+
+// 1. The client sends a request to the server to run the method in a secure environment, just like an AJAX request would work
+
+// 2. A simulation of the method runs directly on the client to attempt to predict the outcome of the server call using the available information
+
+// What this means is that a newly created tasks actually appears on the screen before the result comes back from the server. If the result from the server comes back and is consistent with the simulation on the client, everything remains as is. If the result on the server is different from the result of the simulation on the client, the UI is patched to reflect the actual state of the server.
